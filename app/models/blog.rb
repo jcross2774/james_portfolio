b76e84096091ad7b915f47e4db3c10ba97119f36
James Cross <jcross2774@gmail.com>
2018-01-25 21:59:20 -0700

    Implemented validations for blogs, portfolios and skills

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :body
+
   def should_generate_new_friendly_id?
     slug.blank? || title_changed?
   end
